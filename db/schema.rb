af87d9fcb925cbc826568bcfb6e44e3b9847f35d
cgungaloo <cgungaloo@yahoo.com>
2016-09-22 19:48:54 +0100

    migrated User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910190851) do
+ActiveRecord::Schema.define(version: 20160922184507) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 20160910190851) do
     t.integer  "source_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
